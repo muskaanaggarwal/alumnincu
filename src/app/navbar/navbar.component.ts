235fa171c75400b7708ddc3e08787151d8741656
itsnikhil <taneja.nikhil03@gmail.com>
2019-06-08 00:14:18 +0530

    sticky navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HostListener } from "@angular/core";
+
 
 @Component({
   selector: 'navbar',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  navbar: HTMLElement;
+  sticky: number;
   constructor() { }
 
   ngOnInit() {
+    this.navbar = document.getElementById("navbar");
+    this.sticky = this.navbar.offsetTop;
+  }
+  @HostListener("window:scroll", ['$event'])
+  onWindowScroll() {
+    //we'll do some stuff here when the window is scrolled
+    this.myFunction();
+  }
+
+  myFunction(): void {
+    if (window.pageYOffset >= this.sticky) {
+      this.navbar.classList.add("sticky")
+    } else {
+      this.navbar.classList.remove("sticky");
+    }
   }
 
 }
