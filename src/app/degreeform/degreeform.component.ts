a97e19d405f56aaea8778efa34c122e9a683c803
muskaanaggarwal <muskaanaggarwal7g@gmail.com>
2019-07-10 12:57:19 +0530

    degreeform

--- src/app/degreeform/degreeform.component.ts
+++ src/app/degreeform/degreeform.component.ts
@@ -11,5 +11,29 @@ export class DegreeformComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  school: string = 'SOET'
+  schools: object = {
+    SOET: {
+      "B.Tech - Computer Science & Engineering (CSE)": "B.Tech - Computer Science & Engineering (CSE)",
+      2: 2,
+      3: 3
+    },
+    "School Of Applied Sciences": {
+      4: 4,
+      5: 5,
+      6: 6
+    },
+    SOM: {
+      7: 7,
+      8: 8,
+      9: 9
+    }
+  }
+  onSelect(key: string) {
+    console.log(key);
+    this.school = key;
+  }
+  onProgram(key: string) {
+    console.log(key);
+  }
 }
