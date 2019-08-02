9d2d55404cfbf2c50c302a096c8815885bf7a225
Prabal Goyal <prabalgoyal1999@gmail.com>
2019-08-02 18:27:17 +0530

    muskanihateyou

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { DataserviceService } from '../dataservice.service';
 import { NgForm, NgModel } from "@angular/forms";
+import { Router } from '@angular/router';
+// import { MustMatch } from './_helpers/must-match.validator';
+
 
 
 
@@ -12,26 +15,46 @@ import { NgForm, NgModel } from "@angular/forms";
 })
 export class SignupComponent implements OnInit {
 
-  
+  errorMessage: string;
+  submitted = false;
   signupForm: FormGroup;
   url = 'http://localhost:9800/signup';
   // title = 'alumnincu';
   
   // Register your dataservice using dependency injection
   
-  constructor(private formBuilder: FormBuilder, private dataService: DataserviceService) { }
+  constructor(private formBuilder: FormBuilder, private dataService: DataserviceService, private route: Router) { }
   ngOnInit() {
+    this.errorMessage = null;
     this.signupForm = this.formBuilder.group({
-      roll_no: ['', Validators.required],
-      email: [''],
-      contact: [''],
-      date_of_birth: [''],
-      first_name: [''],
-      last_name: [''],
+      roll_no: ['',Validators.required],
+      email: ['',Validators.required],
+      contact: ['',Validators.required],
+      date_of_birth: ['',Validators.required],
+      first_name: ['',Validators.required],
+      last_name: ['',Validators.required],
       isverified: [1],
-      password: ['']
-      });
+      password: ['',Validators.required],
+      confirmpassword: ['',Validators.required]
+    // },{
+      // validator: MustMatch('password', 'confirmpassword')
+          });
   }
+
+
+  get f() { return this.signupForm.controls; }
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.signupForm.invalid) {
+        return;
+    }
+
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.signupForm.value))
+}
+
+
   
   signup() {
     console.log("Data before*****", this.signupForm.value)
