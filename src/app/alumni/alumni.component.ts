e8737ec3d11ee7748386fcaecd15a97bca1524af
Prabal Goyal <prabalgoyal1999@gmail.com>
2019-08-03 19:22:28 +0530

    I am not rihanna, i dont love the way you lie

--- src/app/alumni/alumni.component.ts
+++ src/app/alumni/alumni.component.ts
@@ -10,8 +10,10 @@ import { Router } from '@angular/router';
 })
 export class AlumniComponent implements OnInit {
   loginForm: FormGroup;
-  url= 'http://localhost:9800/loginform';
+  url = 'http://localhost:9800/loginform';
   errorMessage: string;
+  submitted = false;
+
 
 
   constructor(private formBuilder: FormBuilder, private dataService: DataserviceService, private route: Router) { }
@@ -19,38 +21,50 @@ export class AlumniComponent implements OnInit {
   ngOnInit() {
     this.errorMessage = null;
     this.loginForm = this.formBuilder.group({
-      roll_no: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
-      password: ['', Validators.compose([Validators.required, Validators.minLength(8)])]
+      roll_no: ['',[Validators.required, Validators.minLength(6)]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
     })
   }
 
-  reset(){
+  reset() {
     this.loginForm.reset();
     this.errorMessage = null;
   }
 
+  get f() { return this.loginForm.controls; }
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+    this.login();
+    //  this.route.navigateByUrl('/alumni');
+  }
+
   login() {
-    if (this.loginForm.valid){
+    if (this.loginForm.valid) {
       this.dataService.login("http://localhost:9800/login", this.loginForm.value).subscribe((data: Array<any>) => {
-        if (data.length){
-          if(data[0]["isverified"] != 1){
+        if (data.length) {
+          if (data[0]["isverified"] != 1) {
             this.errorMessage = "Please wait till your account gets verified!";
           }
-          else{
+          else {
             this.dataService.user = data[0];
             this.route.navigateByUrl('/degreeform');
           }
         }
-        else{
+        else {
           this.errorMessage = "Invalid roll number or password!";
         }
       },
         (error: any) => {
-          this.errorMessage = "Something went wrong!\n"+error['message'];
+          this.errorMessage = "Something went wrong!\n" + error['message'];
           console.log("Error in logging in", error);
         });
-      }
-    else{
+    }
+    else {
       this.errorMessage = "Form invalid!";
     }
   }
