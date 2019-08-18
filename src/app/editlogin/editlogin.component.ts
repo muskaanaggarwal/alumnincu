c45d3426794c4f34a8e3ad71c7f0ae8286094dde
Prabal Goyal <prabalgoyal1999@gmail.com>
2019-08-18 23:35:39 +0530

    popppppopppp

--- src/app/editlogin/editlogin.component.ts
+++ src/app/editlogin/editlogin.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DataserviceService } from '../dataservice.service';
+import { Router } from '@angular/router';
+import { MustMatch } from '../_helpers/must-match.validator';
+
 
 
 @Component({
@@ -8,9 +13,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditloginComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+  submitted = false;
+  url = 'http://localhost:9800/signup';
+
+
+
+  constructor(private formBuilder: FormBuilder, private dataService: DataserviceService, private route: Router) { }
 
   ngOnInit() {
+    this.signupForm = this.formBuilder.group({
+      password: [''],
+      newpassword: ['', [Validators.required, Validators.minLength(8)]],
+      confirmnewpassword: ['', Validators.required]
+  }, {
+      validator: MustMatch('newpassword', 'confirmnewPassword')
+  });
+  }
+
+  
+  get f() { return this.signupForm.controls; }
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.signupForm.invalid) {
+      return;
+    }
+    this.signup();
+     this.route.navigateByUrl('/alumni');
+}
+  signup() {
+    this.dataService.alumniportalUser(this.url, this.signupForm.value).subscribe((data: Array<any>) => {
+    },
+      (error: any) => {
+      });
   }
 
 }
--- src/app/editpersonal/editpersonal.component.ts
+++ src/app/editpersonal/editpersonal.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DataserviceService } from '../dataservice.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-editpersonal',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditpersonalComponent implements OnInit {
 
-  constructor() { }
+  personalForm: FormGroup;
+  errorMessage: string;
+  saved: boolean;
+  successMessage: string;
+  url = 'http://localhost:9800/personal_detailsform';
+
+
+
+  constructor(private formBuilder: FormBuilder, private dataService: DataserviceService, private route: Router) { }
 
   ngOnInit() {
+    this.personalForm = this.formBuilder.group({
+      facebook: [''],
+      linkedin: [''],
+      twitter: [''],
+      spouse_name: [''],
+      anniversary_date: [''],
+
+    });
+  }
+  
+
+  postPersonalDetails() {
+      this.errorMessage = "";
+      if (this.personalForm.valid) {
+        this.personalForm.value['roll_no'] = this.dataService.user['roll_no'];
+        this.personalForm.value['batch_id'] = this.dataService.degreeForm.value['batch_id'];
+        this.personalForm.value['specialization_id'] = this.dataService.degreeForm.value['specialization_id'];
+        this.dataService.alumniportalUser(this.url, this.personalForm.value).subscribe((data: Array<any>) => {
+          this.saved = true;
+          this.successMessage = "Saved successfully! Click next to proceed";
+        },
+          (error: any) => {
+            this.errorMessage = error.message;
+          });
+      }
+      else {
+        this.saved = false;
+        this.errorMessage = "Please fill all the details";
+      }
+    }
   }
 
-}
+
