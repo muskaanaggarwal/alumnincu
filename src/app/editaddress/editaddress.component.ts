4a9ea4fe9dcd7e9da21ca8be8a4b25d3062839ca
nishantkhatri29 <nishantkhatri29@gmail.com>
2019-08-21 22:31:03 +0530

    POST Address Details

--- src/app/editaddress/editaddress.component.ts
+++ src/app/editaddress/editaddress.component.ts
@@ -11,7 +11,6 @@ import { Router } from '@angular/router';
 export class EditaddressComponent implements OnInit {
   
   addressForm: FormGroup;
-  saved: boolean;
   errorMessage: string;
   successMessage: string;
   submitted = false;
@@ -21,9 +20,8 @@ export class EditaddressComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private dataService: DataserviceService, private route: Router) { }
 
   ngOnInit() {
- 
     this.addressForm = this.formBuilder.group({
-      address_line_1: [''],
+      address_line_1: ['', Validators.required],
       address_line_2: [''],
       address_line_3: [''],
       city: [''],
@@ -31,54 +29,32 @@ export class EditaddressComponent implements OnInit {
       country: [''],
       pincode: ['']
     });
-   
-  }
-
-  // ngOnDestroy() {
-  //   this.dataService.addressForm = this.addressForm;
-  // }
-  // checkSaved() {
-  //   if (!this.saved) {
-  //     this.errorMessage = "Please click save before you proceed!"
-  //   }
-  //   else {
-  //     this.route.navigateByUrl('/personalform');
-  //   }
-  // }
-
-  
-
-  get f() { return this.addressForm.controls; }
-  onSubmit() {
-    this.submitted = true;
-
-    // stop here if form is invalid
-    if (this.addressForm.invalid) {
+    if (!this.dataService.user) {
+      this.route.navigateByUrl('/alumni');
       return;
     }
-    this.postAddress();
-}
+    if (this.dataService.addressForm) {
+      this.addressForm = this.dataService.addressForm;
+    }
+  }
+
+  ngOnDestroy() {
+    this.dataService.addressForm = this.addressForm;
+  }
 
   postAddress() {
-    if (!this.saved) {
-      this.errorMessage = "";
-      // if (this.addressForm.valid && this.dataService.user['roll_no'] != undefined) {
-        // this.addressForm.value['roll_no'] = this.dataService.user['roll_no'];
-        // console.log(this.addressForm);
+      if (this.addressForm.valid && this.dataService.user['roll_no'] != undefined) {
+        this.addressForm.value['roll_no'] = this.dataService.user['roll_no'];
         this.dataService.alumniportalUser(this.url, this.addressForm.value).subscribe((data: Array<any>) => {
-          this.saved = true;
           this.successMessage = "Saved successfully! Click next to proceed"
         },
           (error: any) => {
-            // this.errorMessage = error.message;
-            console.log(error);
-
+            this.errorMessage = error.message;
           });
       }
-      // else {
-      //   this.saved = false;
-      //   this.errorMessage = 'Please fill all the details';
-      // }
+      else {
+        this.errorMessage = 'Please fill all the details';
+      }
     }
-  }
-
+  
+}
